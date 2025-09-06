f65d4ffbe83d00034fbe7e1c62ba011184863f34
pezzaliapp <pezzalialessandro@gmail.com>
2025-09-06 20:37:13 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,8 @@
-// placeholder service worker v23
+// SW full v23
+const CACHE='tama-full-v23';
+const ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>{ if(e.request.method!=='GET')return;
+  e.respondWith(fetch(e.request).then(r=>{caches.open(CACHE).then(c=>c.put(e.request,r.clone()));return r;}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))));
+});
